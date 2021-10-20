6d15ee87921de11893eddbf55ec1d41978112f12
shukalova <65778248+shukalova@users.noreply.github.com>
2021-10-20 15:29:50 +0300

    Add project files.

---
+++ Lab2_team1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab2_team1
+{
+    public partial class MainWindow : Window
+    {
+        private List<StudentCampus> copiedStudentCampuses = new List<StudentCampus>();
+        private StudentCampus studentCampus = new StudentCampus();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TBUniName.Text = studentCampus.UniversityName;
+            TBCampusAddress.Text = studentCampus.Address;
+            LbRoomCount.Content = studentCampus.RoomCount;
+            LbStaffCount.Content = studentCampus.RoomCount;
+            LbStudentCount.Content = studentCampus.StudentCount;
+            LbStudentDebt.Content = studentCampus.StudentDebt;
+            LbCalculate.Content = $"Посчитать (тариф {studentCampus.PayPerMonthPerStudent} дол. в мес. на чел.)";
+        }
+        private void TBUniName_TextChanged(object sender, TextChangedEventArgs e) => studentCampus.UniversityName = TBUniName.Text;
+        private void TBCampusAddress_TextChanged(object sender, TextChangedEventArgs e) => studentCampus.Address = TBCampusAddress.Text;
+
+        private void BAddRooms_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBAddRooms.Text.Length == 0) return;
+
+            int tryParseResult;
+            int.TryParse(TBAddRooms.Text, out tryParseResult);
+            TBAddRooms.Text = "";
+
+            if (tryParseResult <= 0)
+            {
+                MessageBox.Show("Не бывает такого числа комнат!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+                
+            studentCampus.AddRooms(tryParseResult);
+            LbRoomCount.Content = studentCampus.RoomCount;
+        }
+        private void BOkStudents_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBStudentMove.Text.Length == 0) return;
+
+            int tryParseResult;
+            int.TryParse(TBStudentMove.Text, out tryParseResult);
+            TBStudentMove.Text = "";
+
+            if (tryParseResult <= 0)
+            {
+                MessageBox.Show("Не бывает такого числа студентов!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if(RBMoveIn.IsChecked == true)
+                studentCampus.MoveStudents(tryParseResult, StudentCampus.StudentMove.MoveIn);
+            else
+                studentCampus.MoveStudents(tryParseResult, StudentCampus.StudentMove.MoveOut);
+
+            LbStudentDebt.Content = studentCampus.StudentDebt;
+            LbStudentCount.Content = studentCampus.StudentCount;
+        }
+        private void BCampusInfo_Click(object sender, RoutedEventArgs e) => TBCampusInfo.Text = studentCampus.ToString();
+        private void BCloneObject_Click(object sender, RoutedEventArgs e)
+        {
+            copiedStudentCampuses.Add(studentCampus.Clone() as StudentCampus);
+            MessageBox.Show("Копия успешно создана!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void BAddCafeteria_Click(object sender, RoutedEventArgs e)
+        {
+            MyExtentions.AddCafeteria(studentCampus);
+            MessageBox.Show("Столовая успешно добавлена!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            LbStaffCount.Content = studentCampus.StaffCount;
+            LbCalculate.Content = $"Посчитать (тариф {studentCampus.PayPerMonthPerStudent} дол. в мес. на чел.)";
+            LbStudentDebt.Content = studentCampus.StudentDebt;
+        }
+        private void BСCalculateProfit_Click(object sender, RoutedEventArgs e)
+        {
+            switch (ComboBox.SelectedIndex)
+            {
+                case 0: LbStudentDebtForPeriod.Content = studentCampus.CalculateProfit(StudentCampus.Period.Month); break;
+                case 1: LbStudentDebtForPeriod.Content = studentCampus.CalculateProfit(StudentCampus.Period.SixthMonths); break;
+                case 2: LbStudentDebtForPeriod.Content = studentCampus.CalculateProfit(StudentCampus.Period.Year); break;
+            }
+        }
+    }
+}
---
+++ Lab2_team1/Student Campus.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Lab2_team1
+{
+    class StudentCampus: ICloneable
+    {
+        private string _universityName = "";
+        private string _address = "";
+        private int _roomCount = 0;
+        private int _staffCount = 0;
+        private int _studentCount = 0;
+        private int _studentDebt = 0; //оплата за проживание
+        
+        private int _payPerMonthPerStudent;
+        public enum Period { Month, SixthMonths, Year};
+        public enum StudentMove { MoveIn, MoveOut };
+        private enum PriceLimits { MinPrice = 0, MaxPrice = 1000 };
+
+        public string UniversityName
+        {
+            get { return _universityName; }
+            set { _universityName = value.Length > 0 ? value : "University name"; }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value.Length > 0 ? value : "Campus address address"; }
+        }
+        public int RoomCount
+        {
+            get { return _roomCount; }
+            set { _roomCount = value > 0 ? value : 0; }
+        }
+        public int StaffCount
+        {
+            get { return _staffCount; }
+            set { _staffCount = value > 0 ? value : 0; }
+        }
+        public int StudentCount
+        {
+            get { return _studentCount; }
+            set { _studentCount = value > 0 ? value : 0; Countdebt(); }
+        }
+        public int StudentDebt
+        {
+            get { return _studentDebt; }
+            set { _studentDebt = value > 0 ? value : 0; }
+        }
+        public int PayPerMonthPerStudent
+        {
+            get { return _payPerMonthPerStudent; }
+            set { _payPerMonthPerStudent = value > 0 ? (value > (int)PriceLimits.MaxPrice ? (int)PriceLimits.MaxPrice : value) : 0; Countdebt(); }
+        }
+
+        public void AddRooms(int roomCount) => RoomCount += roomCount;
+        public void MoveStudents(int numberOfStudents, StudentMove studentMove)
+        {
+            switch(studentMove)
+            {
+                case StudentMove.MoveIn: StudentCount += numberOfStudents; break;
+                case StudentMove.MoveOut: StudentCount -= numberOfStudents; break;
+            }
+        }
+        public StudentCampus(string universityName = "", string address = "", int roomCount = 0, int staffCount = 0, int studentCount = 0)
+        {
+            UniversityName = universityName;
+            Address = address;
+            RoomCount = roomCount;
+            StaffCount = staffCount;
+            StudentCount = studentCount;
+
+            Random random = new Random();
+            PayPerMonthPerStudent = random.Next((int)PriceLimits.MinPrice, (int)PriceLimits.MaxPrice);
+        }
+        public override string ToString() =>
+            $"Student campus is named {UniversityName}, its address - {Address}.\n" +
+            $"It has {RoomCount} rooms, {StudentCount} students, and {StaffCount} staff members.\n" +
+            $"Students owe {StudentDebt} dollars to the university.";
+        public object Clone() => base.MemberwiseClone();
+        public int CalculateProfit(Period period)
+        {
+            int sumOfMoney = 0;
+
+            switch(period)
+            {
+                case Period.Month: sumOfMoney = PayPerMonthPerStudent * StudentCount; break;
+                case Period.SixthMonths: sumOfMoney = 6 * PayPerMonthPerStudent * StudentCount; break;
+                case Period.Year: sumOfMoney = 12 * PayPerMonthPerStudent * StudentCount; break;
+            }
+
+            return sumOfMoney;
+        }
+
+        private void Countdebt() => StudentDebt = StudentCount * PayPerMonthPerStudent;
+    }
+    static class MyExtentions
+    {
+        public static void AddCafeteria(this StudentCampus studentCampus)
+        {
+            studentCampus.PayPerMonthPerStudent = (int)(1.2 * studentCampus.PayPerMonthPerStudent);
+            studentCampus.StaffCount += 5;
+        }
+    }
+}
